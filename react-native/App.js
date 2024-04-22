ad0e9159bf562fa1436e77f64ac2f8673ab3ea8c
shehryar-munir <shehryar.munir@dubizzlelabs.com>
2024-04-22 13:47:08 +0500

    Implemented a suggested Change

--- react-native/App.js
+++ react-native/App.js
@@ -3,11 +3,19 @@ import 'react-native-gesture-handler';
 import {ContactsScreen} from './src/screens/ContactsScreen';
 import {Provider} from 'react-redux';
 import store from './src/redux-store/store';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ContactsScreen />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name={'Main'} component={ContactsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
